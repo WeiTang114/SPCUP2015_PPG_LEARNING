6184f9b15a995654a697dfc63870915e9fce64c4
Tang Lee <andy135793726@gmail.com>
2014-10-04 16:16:11 +0800

    Add extract_features
    This extract features from every file of data, and store the features
    seperately into a file "features.mat"

---
+++ extract_features.m
@@ -0,0 +1,14 @@
+% This extract features from every file of data, and
+% store the features seperately into a file "features.mat"
+
+data_idxes = 1:5;
+
+for i = data_idxes
+   [sig, ground_truth] = get_data(i);
+   win_count = size(ground_truth,1);
+   [~,features] = fft_feature(ground_truth, sig);
+   %features 3D: (window, channel, feature(frequency))
+   eval(sprintf('features%d = features', i));
+   save('features.mat',sprintf('features%d', i), '-append');
+end
+

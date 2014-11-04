f0eab9d6270bca11b1d04b7edb4cf634549c1b8f
WeiTang114 <weitang114@gmail.com>
2014-11-05 00:47:04 +0800

    ground_truths and features check exist
    Avoid re-open .mat features and groundtruths files

--- extract_features.m
+++ extract_features.m
@@ -13,13 +13,21 @@ if (exist(filename, 'file') == 0)
 end
 
 for i = g_data_idxes
-   [sig, ground_truth] = get_data(i);
-   win_count = size(ground_truth,1);
-   [~,features] = fft_feature(ground_truth, sig);
-   %features 3D: (window, channel, feature(frequency))
-   eval(sprintf('features%d = features;', i));
-   fprintf(1,'save features%d\n', i); % display formatted string
-   save(filename,sprintf('features%d', i), '-append');
+    
+    % check if the features exists
+    features_i = sprintf('features%d', i);
+    eval(sprintf('global %s', features_i));
+    if exist(features_i, 'var')  ~= 0
+        continue;
+    end
+        
+    [sig, ground_truth] = get_data(i);
+    win_count = size(ground_truth,1);
+    [~,features] = fft_feature(ground_truth, sig);
+    %features 3D: (window, channel, feature(frequency))
+    eval(sprintf('features%d = features;', i));
+    fprintf(1,'save features%d\n', i); % display formatted string
+    save(filename,sprintf('features%d', i), '-append');
 end
 
 clearvars data_idxes features ground_truth i sig win_count filename
--- save_groundtruths.m
+++ save_groundtruths.m
@@ -10,9 +10,17 @@ if (exist(filename, 'file') == 0)
 end
 
 for i = g_data_idxes
-   [sig, ground_truth] = get_data(i);
-   eval(sprintf('ground_truth%d = ground_truth;', i));
-   save(filename,sprintf('ground_truth%d', i), '-append');
+    
+    % check if the ground_truths exists
+    gt_i = sprintf('ground_truth%d', i);
+    eval(sprintf('global %s', gt_i));
+    if exist(gt_i, 'var')  ~= 0
+        continue;
+    end
+    
+    [sig, ground_truth] = get_data(i);
+    eval(sprintf('ground_truth%d = ground_truth;', i));
+    save(filename,sprintf('ground_truth%d', i), '-append');
 end
 
 clearvars data_idxes ground_truth i sig filename

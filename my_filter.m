617c36103eec85e95cd3f301b8f896be00715bca
WeiTang114 <weitang114@gmail.com>
2014-12-09 21:05:51 +0800

    Fix bug: removing "pi"  for cut-off freq calculation.
    cutoff frequency = 0.5 for high or bandpass filter seems good

--- my_filter.m
+++ my_filter.m
@@ -1,12 +1,14 @@
 function [sigout] = my_filter(sigin, fcut, fsample, type)
-% fcut: unnormalized cutoff frequency ( where the signal amplitude is -3dB
-% ). for our work, 0.05 with type = 'high' seems to filter g well.
+% fcut: a scalar or a 2-element vector. Unnormalized cutoff frequency ( where the signal amplitude is -3dB
+%       ). for low/high-pass filter, this is a scalar; for band-pass filter, this
+%       is a 2-element vector. 
 % fsample: sampling frequency, e.g. 125 for our work.
 % type: 'high' for high-pass filter
 %       'low' for low-pass filter
-    
+%       'bandpass' for band-pass filter 
+
     order = 6;
-    [b, a] = butter(order, 2 * pi * fcut / fsample, type);
+    [b, a] = butter(order, 2 * fcut / fsample, type);
     sigout = filter(b, a, sigin);
    
 end

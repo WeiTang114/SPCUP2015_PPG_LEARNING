44e531a3ada9f8796febdbdcacd0011e097d139e
WeiTang114 <weitang114@gmail.com>
2014-12-24 16:52:11 +0800

    Add "grid" function to find the best c and gamma
    run my_svm_grid(1:5) to get the best c and gamma.

--- my_svm_train_cross.m
+++ my_svm_train_cross.m
@@ -1,13 +1,19 @@
-function [mse, corr_coeff] = my_svm_train_cross(training_file, c, gamma)
-    cmd = sprintf('svm-train -s 3 -t 2 -v 5 -q -c %f -g %f %s', c, gamma, training_file);
-    [status, cmdout] = system(cmd);
-    if (status ~= 0)
-        fprintf(1, 'svm train with c=%f g=%f file=%s  failed', c, gamma, training_file);
-        mse = 0;
-        corr_coeff = 0;
-        return;
+function [mse, corr, aae] = my_svm_train_cross(c, gamma, idxes)
+    tmpfile = 'exp\\trainingcross.tmp';
+    tmpfile1 = 'exp\\trainingcross1.tmp';
+    tmpfile2 = 'exp\\trainingcross2.tmp';
+    
+    mse_sum = 0;
+    corr_sum = 0;
+    aae_sum = 0;
+    for i = idxes
+        model = my_svm_train(tmpfile, c, gamma, setdiff(idxes, i));
+        [msei, corri, aaei, ~, ~]  = my_svm_predict(model, tmpfile1, tmpfile2, i);
+        mse_sum = mse_sum + msei;
+        corr_sum = corr_sum + corri;
+        aae_sum = aae_sum + aaei;
     end
-    resultscell = regexp(cmdout, '[0-9.]*', 'match');
-    mse = str2num(char(resultscell(1)));
-    corr_coeff = str2num(char(resultscell(2)));
+    mse = mse_sum / size(idxes, 2);
+    corr = corr_sum / size(idxes, 2);
+    aae = aae_sum / size(idxes, 2);
 end

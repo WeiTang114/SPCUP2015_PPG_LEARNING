fecfd8c2b6920bb338aac9ea8eb1c28417224a9d
WeiTang114 <weitang114@gmail.com>
2015-01-27 13:15:34 +0800

    fft_features: periodogram for ppg

--- fft_feature.m
+++ fft_feature.m
@@ -24,8 +24,14 @@ function [ dim , feature ] = fft_feature( GT , SIG )
         [sig_part(1,:),~,~] = my_ssa(sig_part(1,:), sig_part(3:5, :), L);
         [sig_part(2,:),~,~] = my_ssa(sig_part(2,:), sig_part(3:5, :), L);
         
+        %FFT
+        %fft_sig_part = abs(fft(sig_part,[],2));
         
-        fft_sig_part = abs(fft(sig_part,[],2));
+        %Periodogram
+        for i = 1:5
+            fft_sig_part(i,:) = periodogram(sig_part(i, :), rectwin(window_size), window_size, fps);
+        end
+        
         
         feature(c,:,:) = fft_sig_part(:,dim_low:dim_high);
         for f = 1 : 5
